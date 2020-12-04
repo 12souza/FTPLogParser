782d2eaeb75ae2d6c34796b74609fb0865462418
12souza <32649042+12souza@users.noreply.github.com>
2020-12-04 15:46:17 -0800

    Update ftpstats.py

--- ftpstats.py
+++ ftpstats.py
@@ -11,10 +11,9 @@ from discord.utils import get
 with open('variables.json') as f:
     variables = json.load(f)
 client = commands.Bot(command_prefix = "!", case_insensitive=True)
-ftp = FTP(variables['FTPHOST'], user = variables['USER'], passwd= variables['PASSWORD'])
-ftp.cwd(variables['ftppath'])
 
-def uploadThis(path):
+
+def uploadThis(path, ftp):
     files = os.listdir(path)
     os.chdir(path)
     for f in files:
@@ -25,69 +24,95 @@ def uploadThis(path):
         elif os.path.isdir(path + r'\{}'.format(f)):
             ftp.mkd(f)
             ftp.cwd(f)
-            uploadThis(path + r'\{}'.format(f))
+            uploadThis(path + r'\{}'.format(f), ftp)
     ftp.cwd('..')
     os.chdir('..')
 
 @client.command(pass_context=True,)
 @commands.has_role('Sanitation Engineers')
 async def stats(ctx):
-    #ftp = FTP(variables['FTPHOST'], user = variables['USER'], passwd= variables['PASSWORD'])
-    #ftp.cwd(variables['ftppath'])
+    ftp = FTP(variables['FTPHOST'], user = variables['USER'], passwd= variables['PASSWORD'])
+    ftp.cwd(variables['ftppath'])
     await ctx.send("Foomalyzing and parsing logs, this may take some time..")
     roundOneLink = ""
     roundTwoLink = ""
-    path = variables['path']  
+    logToParse = ""
+    path = variables['path']
+    c = -1  
 
     imagePath = variables['imagepath']  
-    stylePath = variables['stylepath'] 
-
+    stylePath = variables['stylepath']
+    print(ftp.nlst())
     dirList = ftp.nlst()
     logs = []
     for i in dirList:
         if(".log" in i):
             logs.append(i)
-    logToParse = (logs[-3])
-    parseFolder = logToParse[:-4]
-    ftp.retrbinary("RETR " + logToParse, open(logToParse, 'wb').write)
+    for i in range(len(logs)):
+        print(c)
+        if(int(ftp.size(logs[c])) > 100000):
+            print(int(ftp.size(logs[c])))
+            print(logs[c])
+            logToParse = logs[c]
+            break
+        else:
+            c = c - 1
+    
+    print(logToParse)
+    print(ftp.pwd())
 
     parseFolder = logToParse[:-4]
-    roundOneLink = variables["URLPATH"] + parseFolder + "/"
-    os.mkdir(parseFolder)
-    desPath = variables['despath'] + parseFolder
+    if (parseFolder in dirList):
+        print("Folder already exists, no upload necessary")
+        roundTwoLink = variables["URLPATH"] + parseFolder + "/"
+    else:
+        ftp.retrbinary("RETR " + logToParse, open(logToParse, 'wb').write)
+        roundTwoLink = variables["URLPATH"] + parseFolder + "/"
+        os.mkdir(parseFolder)
+        desPath = variables['despath'] + parseFolder
 
-    os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder, logs[-3]))
-    ftp.mkd(parseFolder)
-    ftp.cwd(parseFolder)
-    print(ftp.pwd(), desPath)
-    uploadThis(desPath)
-    #ftp.cwd("../")
-    os.remove(logToParse)
+        os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder, logToParse))
+        ftp.mkd(parseFolder)
+        ftp.cwd(parseFolder)
+        uploadThis(desPath, ftp)
+        os.remove(logToParse)
+        shutil.rmtree(path + parseFolder)
     
 
-    print(ftp.pwd()) 
-    logToParse = (logs[-2])
+    #print(ftp.pwd())
+    c = c - 1 
+    for i in range(len(logs)):
+        print(c)
+        if(int(ftp.size(logs[c])) > 100000):
+            print(int(ftp.size(logs[c])))
+            print(logs[c])
+            logToParse = logs[c]
+            break
+        else:
+            c = c - 1
+
+    print(logToParse)
     parseFolder = logToParse[:-4]
+    if(parseFolder in dirList):
+        print("Folder already exists, no upload necessary")
+        roundOneLink = variables["URLPATH"] + parseFolder + "/"
+    else:   
+        ftp.retrbinary("RETR " + logToParse, open(logToParse, 'wb').write)
+        roundOneLink = variables["URLPATH"] + parseFolder + "/"
+        os.mkdir(parseFolder)
+        desPath = variables['despath'] + parseFolder
 
-    ftp.retrbinary("RETR " + logToParse, open(logToParse, 'wb').write)
-
-    
-    roundTwoLink = variables["URLPATH"] + parseFolder + "/"
-    os.mkdir(parseFolder)
-    desPath = variables['despath'] + parseFolder
-
-    os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder, logs[-2]))
-    ftp.mkd(parseFolder)
-    ftp.cwd(parseFolder)
-    print (ftp.pwd())
-    uploadThis(desPath)
-    ftp.cwd("../")
-
-    os.remove(logToParse)
+        os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder, logToParse))
+        ftp.mkd(parseFolder)
+        ftp.cwd(parseFolder)
+        uploadThis(desPath, ftp)
+        os.remove(logToParse)
+        print(path + parseFolder)
+        shutil.rmtree(path + parseFolder)
 
     await ctx.send("Round 1: " + roundOneLink)
     await ctx.send("Round 2: " + roundTwoLink)
-
     ftp.quit()
+
 client.run(variables['TOKEN'])
 
