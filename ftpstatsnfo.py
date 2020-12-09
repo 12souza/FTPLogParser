b1f8d7fa16f499f6e6857fe760caabf3b134b1eb
12souza <32649042+12souza@users.noreply.github.com>
2020-12-09 08:41:41 -0800

    Add files via upload

--- ftpstatsnfo.py
+++ ftpstatsnfo.py
@@ -47,12 +47,14 @@ async def stats(ctx):
     dirList = ftp.nlst()
     logs = []
     c = -1
+    print(dirList)
     for i in dirList:
         if(".log" in i):
             logs.append(i)
     for i in range(len(logs)):
         if(int(ftp.size(logs[c])) > 100000):
             logToParse2 = logs[c]
+            print(logToParse2)
             break
         else:
             c = c - 1
@@ -60,6 +62,7 @@ async def stats(ctx):
     for i in range(len(logs)):
         if(int(ftp.size(logs[c])) > 100000):
             logToParse1 = logs[c]
+            print(logToParse1)
             break
         else:
             c = c - 1
@@ -83,7 +86,7 @@ async def stats(ctx):
         os.mkdir(parseFolder1)
         desPath = variables['despath'] + parseFolder1
 
-        os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder1, logToParse1))
+        os.system('cmd /c ' + variables['DRIVE'] + ' && cd %s && logalyzer -image %s -style %s -o %s %s' % (path, imagePath, stylePath, parseFolder1, logToParse1))
         ftp.mkd(parseFolder1)
         ftp.cwd(parseFolder1)
         uploadThis(desPath, ftp)
@@ -98,7 +101,89 @@ async def stats(ctx):
         os.mkdir(parseFolder2)
         desPath = variables['despath'] + parseFolder2
 
-        os.system('cmd /c "F: && cd %s && logalyzer -image %s -style %s -o %s %s"' % (path, imagePath, stylePath, parseFolder2, logToParse2))
+        os.system('cmd /c ' + variables['DRIVE'] + ' && cd %s && logalyzer -image %s -style %s -o %s %s' % (path, imagePath, stylePath, parseFolder2, logToParse2))
+        ftp.mkd(parseFolder2)
+        ftp.cwd(parseFolder2)
+        uploadThis(desPath, ftp)
+        os.remove(logToParse2)
+        shutil.rmtree(path + parseFolder2)
+
+    await ctx.send("Round 1: " + roundOneLink)
+    await ctx.send("Round 2: " + roundTwoLink)
+
+    ftp.quit()
+
+@client.command(pass_context=True,)
+async def hstats(ctx, date, nme, mapname):
+    await ctx.send("Creating and Foomalyzing server logs.. this may take at least 1 minute...")    
+    logToParse1 = ""
+    logToParse2 = ""
+    roundOneLink = ""
+    roundTwoLink = ""
+    path = variables['path']
+    imagePath = variables['imagepath']  
+    stylePath = variables['stylepath']
+
+    ftp = FTP(variables['FTPHOST'], user = variables['USER'], passwd= variables['PASSWORD'])
+    ftp.cwd(variables['ftppath'])
+    dirList = ftp.nlst()
+    logs = []
+    c = -1
+    print(dirList)
+    for i in dirList:
+        if(".log" in i):
+            logs.append(i)
+    for i in range(len(logs)):
+        if(int(ftp.size(logs[c])) > 100000):
+            logToParse2 = logs[c]
+            print(logToParse2)
+            break
+        else:
+            c = c - 1
+    c = c - 1
+    for i in range(len(logs)):
+        if(int(ftp.size(logs[c])) > 100000):
+            logToParse1 = logs[c]
+            print(logToParse1)
+            break
+        else:
+            c = c - 1
+    parseFolder1 = date + "-" + nme + "-" + mapname + "-rd1"
+    parseFolder2 = date + "-" + nme + "-" + mapname + "-rd2"
+    ftp.retrbinary("RETR " + logToParse1, open(logToParse1, 'wb').write)
+    ftp.retrbinary("RETR " + logToParse2, open(logToParse2, 'wb').write)
+    ftp.quit()
+
+    ftp = FTP(variables['SITEHOST'], user = variables['USER2'], passwd= variables['PASSWORD2'])
+    ftp.cwd('public')
+    ftp.cwd('logs')
+    print(ftp.nlst())
+    siteList = ftp.nlst()
+
+    if (parseFolder1 in siteList):
+        print("Folder already exists, no upload necessary")
+        roundOneLink = variables["URLPATH"] + parseFolder1 + "/"
+    else:
+        roundOneLink = variables["URLPATH"] + parseFolder1 + "/"
+        os.mkdir(parseFolder1)
+        desPath = variables['despath'] + parseFolder1
+
+        os.system('cmd /c ' + variables['DRIVE'] + ' && cd %s && logalyzer -image %s -style %s -o %s %s' % (path, imagePath, stylePath, parseFolder1, logToParse1))
+        ftp.mkd(parseFolder1)
+        ftp.cwd(parseFolder1)
+        uploadThis(desPath, ftp)
+        os.remove(logToParse1)
+        shutil.rmtree(path + parseFolder1)
+
+    if (parseFolder2 in siteList):
+        print("Folder already exists, no upload necessary")
+        roundTwoLink = variables["URLPATH"] + parseFolder2 + "/"
+    else:
+        roundTwoLink = variables["URLPATH"] + parseFolder2 + "/"
+        os.mkdir(parseFolder2)
+        desPath = variables['despath'] + parseFolder2
+
+        os.system('cmd /c ' + variables['DRIVE'] + ' && cd %s && logalyzer -image %s -style %s -o %s %s' % (path, imagePath, stylePath, parseFolder2, logToParse2))
         ftp.mkd(parseFolder2)
         ftp.cwd(parseFolder2)
         uploadThis(desPath, ftp)
